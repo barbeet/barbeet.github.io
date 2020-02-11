b9195ca3c4a472dafe378f6c3c58e697b2e26a97
barbeet <59886126+barbeet@users.noreply.github.com>
2020-02-11 22:00:18 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,27 @@
-$('#toggle').click(function(){
+$('.buttonmore2').click(function()
+{
   console.log('click');
-  $('.navigationbox').toggleClass('navigationbox-active');
-  $('.navtoggle').toggleClass('.navtoggle-active');
+  $('.tovarbox2').toggleClass('tovarbox2-active');
+    $('.header').toggleClass('header-active');
+      $('.buttonmore2').toggleClass('buttonmore2-active');
+        $('.nametovar').toggleClass('nametovar-active');
+          $('.pricetovar').toggleClass('pricetovar-active');
+            $('.opisanietovar').toggleClass('opisanietovar-active');
+              $('.infotovar').toggleClass('infotovar-active');
+                $('.moreopisanietovar').toggleClass('moreopisanietovar-active');
+
+
 
 })
+$('.buttonmore1').click(function()
+{
+  console.log('click');
+  $('.tovarbox1').toggleClass('tovarbox1-active');
+    $('.header').toggleClass('header-active');
+      $('.buttonmore1').toggleClass('buttonmore1-active');
+        $('.nametovar').toggleClass('nametovar-active');
+          $('.pricetovar').toggleClass('pricetovar-active');
+            $('.opisanietovar').toggleClass('opisanietovar-active');
+              $('.infotovar').toggleClass('infotovar-active');
+                $('.moreopisanietovar').toggleClass('moreopisanietovar-active');
+                })
